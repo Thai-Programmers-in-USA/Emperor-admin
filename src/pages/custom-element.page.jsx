a70d05476d3433c2cbe2de6ed30fab74e4cb2a0f
NUTT CHOKWITTAYA <15667217+ACHEER1420@users.noreply.github.com>
2021-01-09 14:21:56 -0600

    add element to custom element page

--- src/pages/custom-element.page.jsx
+++ src/pages/custom-element.page.jsx
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
+import ButtonDark from '../components/button-dark.component';
 
 function CustomElement(props) {
   return (
     <Fragment>
       <h1>Custom elements</h1>
+      <h2>button-dark</h2>
+      <ButtonDark>LOG IN</ButtonDark>
     </Fragment>
   );
 }
